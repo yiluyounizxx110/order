9f2bfe6db3efdcba05fc51c768e9378a24138cc9
zhuxingxing <990722162@qq.com>
2017-05-13 17:22:16 +0800

    上市时间

--- js/list.js
+++ js/list.js
@@ -22,9 +22,11 @@ function bindEvent(){
 	});
 	$(".category_opr_btn.mul_sel").click(function(){
 		var parent= $(this).parents(".category_item");
+		var ul_sc = parent.find(".category_item_list").first();
 		if(!parent.hasClass("show_all")){
 			parent.addClass("show_all");
 		}
+		ul_sc.addClass("m_h_160");
 		parent.find(".category_item_list").addClass("multi_check");
 		parent.find(".category_opr").addClass("none");
 		parent.find(".multi_check_info").removeClass("none");
@@ -53,9 +55,11 @@ function mutilCheckClick(obj,event){
 }
 function cancelMutilCheck(obj){
 	var parent= $(obj).parents(".category_item").first();
+	var ul_sc = parent.find(".category_item_list").first();
 	if(parent.hasClass("show_all")){
 		parent.removeClass("show_all");
 	}
+	ul_sc.addClass("m_h_160");
 	parent.find(".category_item_list").removeClass("multi_check");
 	parent.find(".category_opr").removeClass("none");
 	parent.find(".multi_check_info").addClass("none");
